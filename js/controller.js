7df31ebb8d6447936edb7feab2e631576f396580
andresDeth <andres.arevalopar@gmail.com>
2015-07-26 16:16:08 -0500

    Inclusion de funcionalidad JS
    Se agrego un archivo controlador de Javascript donde se empieza a
    utilizar todas las funcionalidad de phonegap con el teléfono.
    Bluetooth, agenda, NFC, etc.

---
+++ js/controller.js
@@ -0,0 +1,12 @@
+
+ // $(document).ready(function(){
+ //   console.log("Hola");
+ //  });
+
+function OnBodyLoad(){
+   document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+function onDeviceReady(){
+  alert("OK");
+}
