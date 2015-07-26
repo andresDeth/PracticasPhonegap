24b8407fd8cb2ad15a4bf1db6b17dc28485209ce
andresDeth <andres.arevalopar@gmail.com>
2015-07-26 18:34:59 -0500

    Aplicación Acelerometro
    Se agrego el Phonegap.js, se configuro el config.xml para agregar el
    plugin de acelerometro a la aplicación.

--- js/controller.js
+++ js/controller.js
@@ -4,9 +4,25 @@
  //  });
 
 function OnBodyLoad(){
-   document.addEventListener("deviceready", onDeviceReady, false);
+   //document.addEventListener("deviceready", onDeviceReady, false);
+   onDeviceReady();
 }
 
+
 function onDeviceReady(){
-  alert("OK");
+  //Acelerometro: Ejecuta una unica vez
+	//navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+  //Ejecuta en periodos de tiempo repetitivamente
+  var watch= navigator.accelerometer.watchAcceleration(onSuccess, onError,{frequency: 1000});
+}
+
+function onSuccess(acceleration) {
+	$("#txt_acelerometro").html('<strong>Acceleration X: </strong><p>' + acceleration.x + '</p>' +
+		'<strong>Acceleration Y: </strong><p>' + acceleration.y + '</p>' +
+		'<strong>Acceleration Z: </strong><p>' + acceleration.z + '</p>' +
+        '<strong>Timestamp: </strong><p>' + acceleration.timestamp + '</p>');
+}
+
+function onError(tx,error) {
+    alert('onError!'+error);
 }
