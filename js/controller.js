3db57d32c31403640a7102f10fb93f44aa571031
andresDeth <andres.arevalopar@gmail.com>
2015-07-26 20:33:48 -0500

    Funcionalidad reproducir audio
    Se agrego un archivo a una pagina de internet con un archivo de audio.
    Cuando se pulse en el tercer botón de la barra inferior de la pagina
    principal, se reproducira el archivo de audio.

--- js/controller.js
+++ js/controller.js
@@ -31,7 +31,11 @@ function onDeviceReady(){
   navigator.notification.beep(2);
   });
 
-   $("#geolocaliza").on('pageinit',function_geo);
+   $("#geolocaliza").on('pageinit',function_geo); //GPS
+
+   $("btn_saveContact").on('vclick',function_Agenda); // Agenda de contactos
+
+   $("btn_StartVideo").on('vclick',function_Media); // Reproductor de video
 }
 
 function onSuccess(acceleration) {
@@ -45,7 +49,7 @@ function onError(tx,error) {
     alert('onError!'+error);
 }
 
-//Funcion que utiliza el GPS
+//Funciones que utiliza el GPS
 function function_geo(){
    $("#resultado4").html("Esperando al GPS");
    
@@ -68,5 +72,76 @@ function onErrorGPS(error){
    navigator.notification.alert("Error GPS, codigo: "+error.code+"  message: "+error.message);
 }
 
+//Funciones que utilizamos para guardar un contacto
+function function_Agenda(evt){
+   evt.preventDefault();
+   evt.stopInmediatePropagation();
+
+   var mycontacto = navigator.contacts.create();
+
+   mycontacto.displayName= "PruebaCont";
+
+   var nombre= new ContactName();
+
+   nombre.familyName = "Linda";
+   nombre.middleName = "L. E.";
+   mycontacto.name= nombre;
+
+   mycontacto.phoneNumbers = [];
+   mycontacto.phoneNumbers[0] = new ContactField("home","7654773");
+
+   mycontacto.save(OnGuardado,OnErrorContact);
+}
+
+function OnGuardado(contacto){
+   alert("Contacto guardado "+contacto);
+}
+
+function OnErrorContact(error){
+	alert("Error "+error.message);
+}
+
+//Funciones utilizadas para reproducir un video
+function function_Media(evt){
+   evt.preventDefault();
+   evt.stopInmediatePropagation();
+    
+   playAudio("http://audio.ibeat.org/?ccm=/api/query/stream.m3u&f=m3u&ids=697");
+}
+
+var media= null;
+var timmer = null;
+
+function playAudio(src){
+	if(media === null){
+       media = new Media(src,OnSuccesAudio,OnErrorAudio); //Busca el audio 
+    }
+   media.play();
+
+   if(timmer === null){
+    timmer = setInterval(
+			function(){
+				media.getCurrentPosition(
+						function(position){
+							if(position > -1){
+								$("#resultadoAudio").html("Pos = "+position);
+							}
+						},
+						function(error){ //Error
+							$("#resultadoAudio").html("Error = "+error);
+							setAudioPosition("Error: "+error);
+						}
+					);
+			}, 1000);
+   }
+}
+
+function OnSuccesAudio(){
+	alert("Audio cargado Ok.");
+}
+
+function OnErrorAudio(error){
+   alert("Error al encontrar audio "+error.code);
+}
 
 
